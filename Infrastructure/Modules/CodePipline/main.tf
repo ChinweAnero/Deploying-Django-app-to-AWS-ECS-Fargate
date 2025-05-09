ac9fe475333e21b7587f5f7b41b11b8026921220
ChinweAnero <chinwendu.anero@yahoo.co.uk>
2025-05-09 19:15:48 +0100

    formating build.spec2

--- Infrastructure/Modules/CodePipline/main.tf
+++ Infrastructure/Modules/CodePipline/main.tf
@@ -75,7 +75,7 @@ resource "aws_codepipeline" "codepipeline" {
       configuration = {
         TaskDefinitionTemplatePath     = "App/taskdef.json"
         AppSpecTemplateArtifact        = "BuildArtifact_backend"
-        AppSpecTemplatePath            = "App/appspec.json"
+        AppSpecTemplatePath            = "appspec.json"
         ApplicationName                = var.AppName_Backend
         DeploymentGroupName            = var.DeploymentGroup_backend
         TaskDefinitionTemplateArtifact = "BuildArtifact_backend"
@@ -94,7 +94,7 @@ resource "aws_codepipeline" "codepipeline" {
       configuration = {
         TaskDefinitionTemplatePath     = "App/taskdef.json"
         AppSpecTemplateArtifact        = "BuildArtifact_frontend"
-        AppSpecTemplatePath            = "App/appspec.json"
+        AppSpecTemplatePath            = "appspec.json"
         ApplicationName              = var.AppName_frontend
         DeploymentGroupName            = var.DeploymentGroup_frontend
         TaskDefinitionTemplateArtifact = "BuildArtifact_frontend"
