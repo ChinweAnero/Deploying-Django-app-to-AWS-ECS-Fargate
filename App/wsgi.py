921e657dcdc2a0eadceb93bc6ba035cd59ab92b6
ChinweAnero <chinwendu.anero@yahoo.co.uk>
2025-05-14 19:08:46 +0100

    fix gunicorn failure2

--- App/wsgi.py
+++ App/wsgi.py
@@ -12,7 +12,7 @@ import sys
 
 sys.path.append(os.path.dirname(os.path.abspath(__file__)) + '/../')
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'App.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings')
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
