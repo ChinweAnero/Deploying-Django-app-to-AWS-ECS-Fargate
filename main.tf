71f305d18627475c6101a0d423bc02d6c7472479
ChinweAnero <chinwendu.anero@yahoo.co.uk>
2025-05-15 17:38:48 +0100

    fixing failing terraform plan

--- main.tf
+++ main.tf
@@ -15,25 +15,24 @@ terraform {
 
 
 
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = "3.6.3"
-    }
-  }
-}
+# terraform {
+#   required_providers {
+#     aws = {
+#       source  = "hashicorp/aws"
+#       version = "~> 5.0"
+#     }
+#     random = {
+#       source  = "hashicorp/random"
+#       version = "3.6.3"
+#     }
+#   }
+# }
 
 # AWS Provider
 provider "aws" {
   region                   = "eu-west-2"
   #shared_credentials_files = ["C:/Users/Chinwe/.aws/credentials"]
-  # access_key = "secrets.AWS_ACCESS_KEY_ID"
-  # secret_key = "secrets.AWS_SECRET_ACCESS_KEY"
+
 
 }
 
