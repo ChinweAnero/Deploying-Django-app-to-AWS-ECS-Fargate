fbde60b4ff32053854b19f9695548a21515bb3e7
ChinweAnero <chinwendu.anero@yahoo.co.uk>
2025-05-15 14:04:11 +0100

    adding backend remote block to templates/main.tf 5

--- Infrastructure/Templates/main.tf
+++ Infrastructure/Templates/main.tf
@@ -1,4 +1,19 @@
 
+# The configuration for the `remote` backend.
+terraform {
+  backend "remote" {
+        # The name of your Terraform Cloud organization.
+    organization = "Chinwe-Org"
+
+        # The name of the Terraform Cloud workspace to store Terraform state files in.
+    workspaces {
+      name = "weather_app_infra_workspace"
+    }
+  }
+}
+
+
+
 terraform {
   required_providers {
     aws = {
