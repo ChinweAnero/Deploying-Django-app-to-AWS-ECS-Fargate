515855e24b03da6bfe35cc9b0534b5152f1e78be
ChinweAnero <chinwendu.anero@yahoo.co.uk>
2025-06-04 16:43:25 +0100

    adding validation for cloudwatch agent

--- Infrastructure/Modules/Cloudwatch_agent/main.tf
+++ Infrastructure/Modules/Cloudwatch_agent/main.tf
@@ -2,28 +2,37 @@ resource "aws_ssm_parameter" "cloudwatch_agent" {
   name = var.cloudwatch_agent_name
   type = var.cloudwatch_agent_type
 
-
   value = jsonencode({
     metrics = {
       metrics_collected = {
         prometheus = {
-          prometheus_config_path = "/opt/aws/amazon-cloudwatch-agent/etc/prometheus.yml",
-
+          prometheus_config_path = "/opt/aws/amazon-cloudwatch-agent/etc/prometheus.yml"
         }
-      },
-      "metrics_destinations" = {
-        "amp" = {
-          "region"       = "eu-west-2",
-          "workspace_id" = var.promethues_workspace_id
-        }
-      },
-      "append_dimensions": {
-      #"TaskId": "aws:ecs:task-id",
-      "ClusterName" = var.clusterName
       }
 
+      metrics_destinations = {
+        amp = {
+          region       = "eu-west-2"
+          workspace_id = var.promethues_workspace_id
+        }
+      }
+
+      append_dimensions = {
+        ClusterName = var.clusterName
+      }
+
+      extensions = {
+        sigv4auth = {
+          service = "aps"
+          region  = "eu-west-2"
+        }
+      }
     }
   })
-}
+  }
+
+
+
+
 
 
