29048c3c504ed5506b0ef401559374e88f389590
ChinweAnero <chinwendu.anero@yahoo.co.uk>
2025-06-10 15:50:16 +0100

    promethues configuration refactoring

---
+++ Infrastructure/Modules/Prometheus Listener/variables.tf
@@ -0,0 +1,6 @@
+variable "prometheus-loadbalancer" {
+  type = string
+}
+variable "prometheus-target-group-arn" {
+  type = string
+}
---
+++ Infrastructure/Modules/Prometheus LoadBalancer/variables.tf
@@ -0,0 +1,7 @@
+
+variable "prometheus-target-group-arn" {
+  type = string
+}
+variable "listener_arn" {
+  type = string
+}
