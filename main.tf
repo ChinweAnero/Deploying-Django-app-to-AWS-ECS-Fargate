e60fd85ab4a8caa498707defb9e37b7e5f7e5f25
ChinweAnero <chinwendu.anero@yahoo.co.uk>
2025-06-11 22:52:26 +0100

    promethues configuration

--- main.tf
+++ main.tf
@@ -144,7 +144,7 @@ module "prometheus_target_group_blue" {
   name   = "prometheus-${var.environment}-target-group-b"
   vpc_id = module.VPC.vpc_id
   create_target_group = true
-  port = 8080
+  port = 9090
   target_type = "ip"
   protocol = "HTTP"
   healthcheck_path = "/-/healthy"
