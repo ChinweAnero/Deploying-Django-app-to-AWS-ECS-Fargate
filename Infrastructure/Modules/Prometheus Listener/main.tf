83147fc634e09940ede1365587a386b4511ee823
ChinweAnero <chinwendu.anero@yahoo.co.uk>
2025-06-13 15:02:54 +0100

    promethues configuration

--- Infrastructure/Modules/Prometheus Listener/main.tf
+++ Infrastructure/Modules/Prometheus Listener/main.tf
@@ -1,14 +1,14 @@
-resource "aws_lb_listener" "prometheus-listener" {
-  #count = 1
-  load_balancer_arn = var.prometheus-loadbalancer
-  port              = "80"
-  protocol          = "HTTP"
-
-  default_action {
-    type             = "forward"
-    target_group_arn = var.prometheus-target-group-arn
-  }
-  lifecycle {
-    ignore_changes = [default_action]
-  }
-}
+# resource "aws_lb_listener" "prometheus-listener" {
+#   #count = 1
+#   load_balancer_arn = var.prometheus-loadbalancer
+#   port              = "443"
+#   protocol          = "HTTPS"
+#
+#   default_action {
+#     type             = "forward"
+#     target_group_arn = var.prometheus-target-group-arn
+#   }
+#   lifecycle {
+#     ignore_changes = [default_action]
+#   }
+#  }
